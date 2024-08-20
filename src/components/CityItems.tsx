e6f8601d4f3aa7d3aa6286dfd9a90e09ce48fa48
stiehle <gstiehle@yahoo.de>
2024-08-20 20:38:23 +0200

    Fix Route

--- src/components/CityItems.tsx
+++ src/components/CityItems.tsx
@@ -2,14 +2,8 @@ import { useState } from "react";
 import "./CityItems.scss";
 
 function CitiesItem() {
-<<<<<<< HEAD
-  const [cities, setCities] = useState<number[]>([576216, 623685, 386789, 267097, 604791, 9000293, 9000433]);
-
-=======
   const [cities] = useState<number[]>([576216, 623685, 386789, 267097, 604791, 9000293, 9000433]);
 
- 
->>>>>>> 11d8fd30e0dc1a538b67288c3e8ede81a9b1ee9e
   return cities.map((city) => {
     return (
       <div className="city-item" key={city}>
